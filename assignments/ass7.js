5039f33680c0dc1e5821fbe98bda1c334c434bdd
simroz-riyaz <riyazsimroz0@gmail.com>
2025-07-10 00:10:07 +0900

    Add files via upload

--- assignments/ass7.js
+++ assignments/ass7.js
@@ -1,14 +1,11 @@
+// if else statement
 
-let subcontinent = 1400000000;
-let India = population / 2; // 700000000
-let Pakistan = population / 2;// 700000000
-console.log(India+1);
-
-let finland = 600000000;
-let isIndiaPopulationLesser = population < averagePopulation;
-console.log(isIndiaPopulationLesser);
-let averagePopulation = 330000000
-let isIndiaPopulationHigher = population > averagePopulation;
-console.log(isIndiaPopulationHigher);
-let description = 'India is in Asia and 7 million people speak Hindi';
-console.log(description);
+let averagePopulation = 330000000;
+const isIndiaPopulation = 340000000;
+if( isIndiaPopulation > averagePopulation){
+    //template literals 
+    console.log(`India's population is above average!`);
+}
+else if (isIndiaPopulation < averagePopulation) {
+    console.log(`India's population is 22 million below average!`);
+}
